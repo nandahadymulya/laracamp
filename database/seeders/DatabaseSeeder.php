94ced0bda74365728b68ccb35abf893c2e0da58b
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2022-02-24 07:02:00 +0700

    create adminUser seeder & call to insert

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             [
                 CampTableSeeder::class,
                 CampBenefitTableSeeder::class,
+                AdminUserSeeder::class,
             ]
         );
     }
